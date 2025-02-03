743ef61b991e0eb59e1a870cdc405b0ae3134672
Shubhaditya <shubh28062000@gmail.com>
2025-02-04 04:13:38 +0530

    second commit

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -10,18 +10,35 @@ const Hero = () => {
 
       <div className="container relative mx-auto px-4 pt-12 pb-24">
         <div className="flex flex-col items-center justify-center text-center z-10">
-<motion.div
-initial={{opacity: 0, scale: 0.5}}
-animate={{opacity: 1, scale: 1}}
-transition={{duration: 0.8}}
-className="relative mb-8 mt-24"
->
-    <img
-    src={profilepic}
-    alt="John Doe"
-    className="w-[250px] relative z-10"
-    />
-</motion.div>
+          <motion.div
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.8 }}
+            className="relative mb-8 mt-24"
+          >
+            <div className="absolute inset-0 bg-gradient-to-b from-emerald-500/20 to-transparent rounded-full blur-3xl"></div>
+            <img
+              src={profilepic}
+              alt="John Doe"
+              className="w-[250px] relative z-10"
+            />
+          </motion.div>
+
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8 }}
+            className="max-w-2xl"
+          >
+            <h1 className="text-6xl md:text-7xl font-bold tracking-tighter mb-4">
+              Hi, I am <br /> Shubhaditya{" "}
+              <span className="text-emerald-400">Singh</span>
+            </h1>
+            <p className="text-xl text-white/80 max-w-lg mx-auto leading-relaxed mb-6">
+              I am a fullstack developer focusing on creating websites that
+              provide users with best experience.
+            </p>
+          </motion.div>
         </div>
       </div>
     </div>
